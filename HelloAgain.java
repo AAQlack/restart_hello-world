4f255b32784d16ffe0fee630ab39c81b2ecdd9d3
东风谷_草田 <62550152+AAQlack@users.noreply.github.com>
2022-09-22 05:06:40 +0800

    Add files via upload

---
+++ HelloAgain.java
@@ -0,0 +1,10 @@
+//重新从helloworld开始
+//注意代码规范:大小驼峰、水平留白
+//有中文注释时记得将编码选为GBK
+public class HelloAgain {
+
+	public static void main(String[] args) {
+		System.out.println("hello,world!");
+		System.out.println("I'm studying java again!");
+	}
+}
